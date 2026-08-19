39daa55f5c41f6ddea8ca133bac43b6b359eac1c
gretagen <gretagen@users.noreply.github.com>
2026-08-20 01:51:10 +0200

    Fix kitty: separate kitty-extensions dir at usr/lib/kitty-extensions

--- packages/kitty/package.lua
+++ packages/kitty/package.lua
@@ -3,7 +3,7 @@ return {
   version = "0.48.2",
   summary = "Fast, feature-rich, GPU-based terminal emulator",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/kitty/kitty-0.48.2.tar.gz",
-  sha256  = "845a0a0e61bffa56455179f5ca0d0af4fa72a2899ce946d011bb1bd146dc46f5",
+  sha256  = "0b5e736b76fe2fc4950e31d02a329ed7797957ef08e06df1d712ab0456d5a02a",
   deps    = {
     "libXcursor", "libXrandr", "libXi", "libXinerama", "libglvnd",
     "libX11", "libxcb", "dbus",
