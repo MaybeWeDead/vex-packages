164c103ccbd69a010054dae400ef38920f5ef55b
gretagen <besternet91@gmail.com>
2026-08-19 16:31:57 +0200

    packages/libXv: add missing X11 Video extension library
    - Fix broken deps: lowercase libxext/libx11 -> libXext/libX11
    - Required by libXvMC (dependency of xf86-video-intel)

---
+++ packages/libXv/package.lua
@@ -0,0 +1,12 @@
+return {
+  name    = "libXv",
+  version = "1.0.13",
+  summary = "X11 Video extension library",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/libXv/libXv-1.0.13.tar.gz",
+  sha256  = "643d19b2f6356c5ad6d9b32715f510d62205038a52a27638de92bac8c1ef36d1",
+  deps    = { "libXext", "libX11", "xorgproto" },
+  archive = { strip = 1 },
+  test    = function(p)
+    p:run("test -f " .. p.install_root .. "/usr/lib/libXv.so")
+  end,
+}
