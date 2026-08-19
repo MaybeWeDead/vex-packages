b78e50c14b188db187b8a43a020b08c1c91c3e6c
gretagen <gretagen@users.noreply.github.com>
2026-08-20 00:27:13 +0200

    Rebuild kitty and xxhash with pre-built binaries

--- packages/kitty/package.lua
+++ packages/kitty/package.lua
@@ -3,7 +3,7 @@ return {
   version = "0.48.2",
   summary = "Fast, feature-rich, GPU-based terminal emulator",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/kitty/kitty-0.48.2.tar.gz",
-  sha256  = "1d32d2985f6f03923ea63fb7c32683b49e2d4ce19aa9e90b161f7f24efb00c9c",
+  sha256  = "353302efde2aff4938b541b506b7982d2bc7665b584b572afab40beac4c93eec",
   deps    = {
     "harfbuzz", "libz", "libpng", "lcms2", "xxhash", "openssl", "pixman",
     "cairo", "freetype", "fontconfig", "libcanberra", "dbus",
