bf12e9617e05b40cba1f329280905c682694f4cc
gretagen <gretagen@users.noreply.github.com>
2026-08-20 00:53:30 +0200

    Fix kitty: place bundled libs in usr/lib for RPATH resolution

--- packages/kitty/package.lua
+++ packages/kitty/package.lua
@@ -3,7 +3,7 @@ return {
   version = "0.48.2",
   summary = "Fast, feature-rich, GPU-based terminal emulator",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/kitty/kitty-0.48.2.tar.gz",
-  sha256  = "353302efde2aff4938b541b506b7982d2bc7665b584b572afab40beac4c93eec",
+  sha256  = "845a0a0e61bffa56455179f5ca0d0af4fa72a2899ce946d011bb1bd146dc46f5",
   deps    = {
     "harfbuzz", "libz", "libpng", "lcms2", "xxhash", "openssl", "pixman",
     "cairo", "freetype", "fontconfig", "libcanberra", "dbus",
