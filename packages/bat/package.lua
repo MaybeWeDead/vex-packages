d5c42a6cb50f2839f3343bf851f4f0832e77d418
zozkide <iqcraft.roblox@gmail.com>
2026-08-20 11:55:11 +0200

    add bat

---
+++ packages/bat/package.lua
@@ -0,0 +1,11 @@
+return {
+	name = "bat",
+	version = "0.26.1",
+	summary = "A cat clone with wings.",
+	url = "https://raw.githubusercontent.com/zozkide/zeta-packages/refs/heads/main/packages/bat/bat-0.26.1.tar.gz",
+	sha256 = "5b8e0e5dcfaefa1b79e3801bd54bdfb4c54053989417ad87ab90bd51226cc830",
+	archive = { strip = 1 },
+	test = function(p)
+		p:run("test -f" .. p.install_root .. "/usr/bin/bat")
+	end,
+}
