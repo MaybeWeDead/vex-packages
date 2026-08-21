2aa3e4686b30f095c85602728a2efa453ac5f797
gretagen <besternet91@gmail.com>
2026-08-22 01:48:40 +0200

    qtbase 6.8.2: add Qt6 plugin path configuration
    - Added /etc/profile.d/qt6.sh (sets QT_PLUGIN_PATH, QT_QPA_PLATFORM_PLUGIN_PATH)
    - Added /etc/qt.conf as fallback path config
    - Fixes: Could not find Qt platform plugin xcb in empty string

--- packages/qtbase/package.lua
+++ packages/qtbase/package.lua
@@ -3,7 +3,7 @@ return {
   version = "6.8.2",
   summary = "Qt 6 base libraries",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/qtbase/qtbase-6.8.2.tar.gz",
-  sha256  = "de2b7bb9ff9506d469df3b328b4b287db346fe8c7f9de23d3f0abfc099dd8376",
+  sha256  = "d3913dd38f9dcddbf6d3980f712e80acf65d8382844298de5364ef4b335fd020",
   deps    = { "at-spi2-core", "cairo", "double-conversion", "fontconfig", "freetype", "gdk-pixbuf", "gtk3", "harfbuzz", "libICE", "libSM", "libb2", "libdrm", "libinput", "libmtdev", "libpng", "libz", "md4c", "pango", "pcre2", "xcb-util-cursor", "xcb-util-image", "xcb-util-keysyms", "xcb-util-renderutil", "xcb-util-wm", "xkbcommon", "zstd" },
   archive = { strip = 1 },
   test    = function(p)
