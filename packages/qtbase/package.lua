2d4b967cf7e603843eb117991db09ca703ddbfe6
gretagen <besternet91@gmail.com>
2026-08-22 01:22:32 +0200

    qtbase 6.8.2: fix plugin install path to /usr/lib/qt6/plugins
    - Moved plugins from /usr/plugins/ to /usr/lib/qt6/plugins/
    - Fixes LXQt crash: libqxcb.so now at correct Qt6 path
    - Fixes: QT platform plugin missing, connection server lost

--- packages/qtbase/package.lua
+++ packages/qtbase/package.lua
@@ -3,7 +3,7 @@ return {
   version = "6.8.2",
   summary = "Qt 6 base libraries",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/qtbase/qtbase-6.8.2.tar.gz",
-  sha256  = "cfa14cd9b84a9ce03368f6a5559c1ea67dd1a181e35fe292e0bfc2040134d7ad",
+  sha256  = "de2b7bb9ff9506d469df3b328b4b287db346fe8c7f9de23d3f0abfc099dd8376",
   deps    = { "at-spi2-core", "cairo", "double-conversion", "fontconfig", "freetype", "gdk-pixbuf", "gtk3", "harfbuzz", "libICE", "libSM", "libb2", "libdrm", "libinput", "libmtdev", "libpng", "libz", "md4c", "pango", "pcre2", "xcb-util-cursor", "xcb-util-image", "xcb-util-keysyms", "xcb-util-renderutil", "xcb-util-wm", "xkbcommon", "zstd" },
   archive = { strip = 1 },
   test    = function(p)
