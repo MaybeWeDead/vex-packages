84e8108c52007d77722f140ee40fc0f2ef049c20
Lucas <lucasp.linux@gmail.com>
2026-08-22 18:32:19 +0200

    add: wine with 64bits and 32bits apps support

---
+++ packages/llvm-mingw/package.lua
@@ -0,0 +1,18 @@
+return {
+  name    = "llvm-mingw",
+  version = "20260616",
+  summary = "LLVM/Clang/LLD based MinGW-w64 cross toolchain",
+  url     = "https://github.com/mstorsjo/llvm-mingw/releases/download/20260616/llvm-mingw-20260616-ucrt-ubuntu-22.04-x86_64.tar.xz",
+  sha256  = "534b92e067b22a6b4441f48ae9240a3341b17825d04d577eab0cf85c44b4deda",
+  arch    = "x86_64",
+  deps    = { "libz", "zstd" },
+  install = function(p)
+    p:install(".", "/opt/llvm-mingw")
+    p:run("mkdir -p " .. p.install_root .. "/usr/bin")
+    p:run("for tool in " .. p.install_root .. "/opt/llvm-mingw/bin/i686-w64-mingw32-* " .. p.install_root .. "/opt/llvm-mingw/bin/x86_64-w64-mingw32-*; do ln -s /opt/llvm-mingw/bin/$(basename \"$tool\") " .. p.install_root .. "/usr/bin/$(basename \"$tool\"); done")
+  end,
+  test    = function(p)
+    p:run(p.install_root .. "/opt/llvm-mingw/bin/i686-w64-mingw32-clang --version")
+    p:run(p.install_root .. "/opt/llvm-mingw/bin/x86_64-w64-mingw32-clang --version")
+  end,
+}
---
+++ packages/wine/package.lua
@@ -0,0 +1,17 @@
+return {
+  name    = "wine",
+  version = "11.0",
+  summary = "Compatibility layer for running Windows applications",
+  url     = "https://dl.winehq.org/wine/source/11.0/wine-11.0.tar.xz",
+  sha256  = "c07a6857933c1fc60dff5448d79f39c92481c1e9db5aa628db9d0358446e0701",
+  arch    = "x86_64",
+  deps    = { "alsa-lib", "dbus", "fontconfig", "freetype", "gnutls", "libX11", "libXcomposite", "libXcursor", "libXext", "libXfixes", "libXi", "libXinerama", "libXrandr", "libXrender", "libxcb", "llvm-mingw", "make", "pulseaudio", "sdl2", "vulkan-loader", "wayland", "xkbcommon" },
+  build   = function(p)
+    p:run("./configure --prefix=/usr --enable-archs=i386,x86_64 --without-oss")
+    p:make()
+    p:make("install", "DESTDIR=" .. p.install_root)
+  end,
+  test    = function(p)
+    p:run(p.install_root .. "/usr/bin/wine --version")
+  end,
+}
