4d3f152b789971b07a183c1a2af311de0012107c
gretagen <besternet91@gmail.com>
2026-08-22 02:46:50 +0200

    qtbase 6.8.2: fix profile.d path and add /etc/environment fallback
    - Moved qt6.sh from ./usr/etc/profile.d/ to ./etc/profile.d/ (standard path)
    - Added Qt6 env vars to /etc/environment as fallback
    - Fixes: /etc/profile.d/qt6.sh No such file or directory

--- packages/qtbase/package.lua
+++ packages/qtbase/package.lua
@@ -3,7 +3,7 @@ return {
   version = "6.8.2",
   summary = "Qt 6 base libraries",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/qtbase/qtbase-6.8.2.tar.gz",
-  sha256  = "d3913dd38f9dcddbf6d3980f712e80acf65d8382844298de5364ef4b335fd020",
+  sha256  = "18f243202e53b408197590d7d491d92e453947c8962a9091836116fe03cf7ae5",
   deps    = { "at-spi2-core", "cairo", "double-conversion", "fontconfig", "freetype", "gdk-pixbuf", "gtk3", "harfbuzz", "libICE", "libSM", "libb2", "libdrm", "libinput", "libmtdev", "libpng", "libz", "md4c", "pango", "pcre2", "xcb-util-cursor", "xcb-util-image", "xcb-util-keysyms", "xcb-util-renderutil", "xcb-util-wm", "xkbcommon", "zstd" },
   archive = { strip = 1 },
   test    = function(p)
