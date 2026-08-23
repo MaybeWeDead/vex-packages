647957d81f8d033c38c7356a438754252e0ea593
gretagen <besternet91@gmail.com>
2026-08-23 20:33:17 +0200

    fetch: add defaults (config, logo, wrapper)

--- packages/fetch/package.lua
+++ packages/fetch/package.lua
@@ -3,10 +3,10 @@ return {
   version = "2.3.0",
   summary = "Animated 3D fetch tool for your terminal",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/fetch/fetch-2.3.0.tar.gz",
-  sha256  = "d7abe52b839f1c3e1c093f5250cf42e50e4384bc0eb71b45b2694755c5ddcc3d",
+  sha256  = "7935ac15a3a6714d94556982f208ac6a57a11b46c80ba0d9eb6f5d2213273f6c",
   deps    = {},
   archive = { strip = 1 },
   test    = function(p)
-    p:run("test -x " .. p.install_root .. "/usr/bin/fetch")
+    p:run("test -x " .. p.install_root .. "/usr/bin/fetch && test -x " .. p.install_root .. "/usr/bin/fetch.bin && test -f " .. p.install_root .. "/usr/share/fetch/config && test -f " .. p.install_root .. "/usr/share/fetch/logo.txt")
   end,
 }
