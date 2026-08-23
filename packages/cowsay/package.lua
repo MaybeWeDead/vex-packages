0b3a4bd4befef6e95c02c6961a07e27ab606767b
vizzion <AyoVizzion@protonmail.com>
2026-08-23 19:48:16 +0000

    Add files via upload

--- packages/cowsay/package.lua
+++ packages/cowsay/package.lua
@@ -1,1 +1,10 @@
-
+return {
+  name    = "cowsay",
+  version = "3.8.4",
+  summary = "Cow says things",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/cowsay/cowsay-3.8.4.tar.gz",
+  sha256  = "a048e90ec3ca14bfaa6f1d4ad789a8d5e4b233cd07ccff8aee15c997152245cb",
+  deps    = {},
+  archive = { strip = 1 },
+  files   = { "usr/bin/cowsay" },
+}
