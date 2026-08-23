a47af2d17a84f97cddc0a641aabf4733913f6b31
gretagen <besternet91@gmail.com>
2026-08-23 02:27:04 +0200

    lua: fix symlinks (relative paths), add headers, clean up binaries

--- packages/lua/package.lua
+++ packages/lua/package.lua
@@ -3,7 +3,7 @@ return {
   version = "5.5.1",
   summary = "Lua 5.5 scripting language interpreter",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/lua/lua-5.5.1.tar.gz",
-  sha256  = "6bba0d17991b7232e695f301262ab6f0d911fce95de77845d1c4667d1391aa6a",
+  sha256  = "cd9d79e0455b26a379ed3a02a72d03c35899919544b2fd0c019bce75c41dab45",
   deps    = {},
   archive = { strip = 1 },
   test    = function(p)
