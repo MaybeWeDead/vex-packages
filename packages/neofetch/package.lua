428a88bcef2efb24c690cde211e65536cb21709f
MaybeWeDead <mmmmbamanciki11@gmail.com>
2026-08-23 18:39:44 +0300

    Update package.lua

--- packages/neofetch/package.lua
+++ packages/neofetch/package.lua
@@ -1,14 +1,11 @@
 return {
   name = "neofetch",
   version = "7.1.0-vexos",
-  summary = "Fast, highly customizable system info script with VexOS logo",
-  url = "https://raw.githubusercontent.com/MaybeWeDead/vex-packages/main/packages/neofetch/neofetch-7.1.0-vexos.tar.gz",
-  sha256 = "cf4bdb3478cb236c507707d51d4cd3ba4a276615886dfd7c1a50b4360da00dae",
+  summary = "Fast, highly customizable system info script",
+  url = "https://githubusercontent.com",
+  sha256 = "6583a3dfecefbd9aaae10c8ceef235030e1d51e5bec2972848c9a2eaceb6ff08",
   deps = {},
   install = function(p)
-    local archive_path = p:fetch()
-    p:unpack(archive_path)
-    p:run("cp neofetch " .. p.install_root .. "/usr/bin/")
-    p:run("chmod +x " .. p.install_root .. "/usr/bin/neofetch")
+    p:install("neofetch", "usr/bin/neofetch")
   end,
 }
