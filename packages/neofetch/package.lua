3912f7e096714cb164a577fc972add9e381abcd4
MaybeWeDead <mmmmbamanciki11@gmail.com>
2026-08-23 16:50:52 +0300

    Update package.lua

--- packages/neofetch/package.lua
+++ packages/neofetch/package.lua
@@ -8,7 +8,7 @@ return {
   install = function(p)
     local archive_path = p:fetch()
     p:unpack(archive_path)
-    p:install(".", "usr/bin/neofetch")
+    p:install("neofetch", "usr/bin/neofetch")
     p:run("chmod +x " .. p.install_root .. "/usr/bin/neofetch")
   end,
 }
