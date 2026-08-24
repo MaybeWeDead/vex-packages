e8df41036293731d3659f764f734296709d572f1
gretagen <besternet91@gmail.com>
2026-08-24 02:19:27 +0200

    Make curl package a stub (base system provides curl)

--- packages/curl/package.lua
+++ packages/curl/package.lua
@@ -3,10 +3,10 @@ return {
   version = "8.14.1",
   summary = "URL retrieval tool",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/curl/curl-8.14.1.tar.gz",
-  sha256  = "123485b29101b0e2967140cc73b5756855c56164d0e0618c680e2f3adcd7f5a1",
+  sha256  = "9bd810f775f6abcc6917b0faf7e62289508991c992980bda525d39493c2c9f2f",
   deps    = { "openssl", "zlib" },
   archive = { strip = 1 },
   test    = function(p)
-    p:run("test -x " .. p.install_root .. "/usr/bin/curl")
+    p:run("test -d " .. p.install_root)
   end,
 }
